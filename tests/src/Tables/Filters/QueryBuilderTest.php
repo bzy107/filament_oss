135c03b13e3168a703131b08c4cbdf555d1f112c
bzy107 <bzy107>
2025-02-25 22:15:35 +0900

    wip

--- tests/src/Tables/Filters/QueryBuilderTest.php
+++ tests/src/Tables/Filters/QueryBuilderTest.php
@@ -10,141 +10,124 @@ use Illuminate\Database\Eloquent\Collection;
 
 uses(TestCase::class);
 
-it('can filter records by text constraint in the query builder', function (Collection $all, Collection $canSee, Collection $canNotSee, string $column, string $operatorName, string $filter = null) {
+
+it('can filter text constraint for `contains`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, 2, 7);
+
     livewire(PostsQueryBuilderTable::class)
-        ->assertCanSeeTableRecords($all)
-        ->queryBuilderTable($column, $operatorName, $filter)
-        ->assertCanSeeTableRecords($canSee)
-        ->assertCanNotSeeTableRecords($canNotSee);
-})
-->with([
-    'contains' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, 2, 7);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'like', '%'. $filter . '%')->get(),
-            'canNotSee' => Post::where('content', 'not like', '%'. $filter . '%')->get(),
-            'column' => 'content',
-            'operatorName' => 'contains',
-            'filter' => $filter,
-        ];
-    },
-    'not contains' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, 2, 7);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'not like', '%' . $filter . '%')->get(),
-            'canNotSee' => Post::where('content', 'like' , '%' . $filter . '%')->get(),
-            'column' => 'content',
-            'operatorName' => 'contains.inverse',
-            'filter' => $filter,
-        ];
-    },
-    'startsWith' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, 0, 5);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'like', $filter . '%')->get(),
-            'canNotSee' => Post::where('content', 'not like', $filter . '%')->get(),
-            'column' => 'content',
-            'operatorName' => 'startsWith',
-            'filter' => $filter,
-        ];
-    },
-    'not startsWith' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, 0, 5);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'not like', $filter . '%')->get(),
-            'canNotSee' => Post::where('content', 'like', $filter . '%')->get(),
-            'column' => 'content',
-            'operatorName' => 'startsWith.inverse',
-            'filter' => $filter,
-        ];
-    },
-    'endsWith' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, -5);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'like', '%' . $filter)->get(),
-            'canNotSee' => Post::where('content', 'not like', '%' . $filter)->get(),
-            'column' => 'content',
-            'operatorName' => 'endsWith',
-            'filter' => $filter,
-        ];
-    },
-    'not endsWith' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        $filter = substr($content, -5);
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', 'not like', '%' . $filter)->get(),
-            'canNotSee' => Post::where('content', 'like', '%' . $filter)->get(),
-            'column' => 'content',
-            'operatorName' => 'endsWith.inverse',
-            'filter' => $filter,
-        ];
-    },
-    'equals' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', $content)->get(),
-            'canNotSee' => Post::where('content', '<>', $content)->get(),
-            'column' => 'content',
-            'operatorName' => 'equals',
-            'filter' => $content,
-        ];
-    },
-    'not equals' => function () {
-        $posts = Post::factory(10)->create();
-        $content = $posts->random()->content;
-        return [
-            'all' => $posts,
-            'canSee' => Post::where('content', '<>', $content)->get(),
-            'canNotSee' => Post::where('content', $content)->get(),
-            'column' => 'content',
-            'operatorName' => 'equals.inverse',
-            'filter' => $content,
-        ];
-    },
-    'isFilled' => function () {
-        Post::factory(8)->create();
-        Post::factory()->create(['content' => null]);
-        Post::factory()->create(['content' => '']);
-        return [
-            'all' => Post::all(),
-            'canSee' => Post::where('content', '<>', null)->where('content', '<>', '')->get(),
-            'canNotSee' => Post::where('content', null)->orWhere('content', '')->get(),
-            'column' => 'content',
-            'operatorName' => 'isFilled',
-        ];
-    },
-    'not isFilled' => function () {
-        Post::factory(8)->create();
-        Post::factory()->create(['content' => null]);
-        Post::factory()->create(['content' => '']);
-        return [
-            'all' => Post::all(),
-            'canSee' => Post::where('content', null)->orWhere('content', '')->get(),
-            'canNotSee' => Post::where('content', '<>', null)->where('content', '<>', '')->get(),
-            'column' => 'content',
-            'operatorName' => 'isFilled.inverse',
-        ];
-    },
-]);
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'contains', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'like', '%' . $filter . '%')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'not like', '%' . $filter . '%')->get());
+});
+
+it('can filter text constraint for `not contains`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, 2, 7);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'contains.inverse', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'not like', '%' . $filter . '%')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'like', '%' . $filter . '%')->get());
+});
+
+it('can filter text constraint for `startsWith`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, 0, 5);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'startsWith', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'like', $filter . '%')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'not like', $filter . '%')->get());
+});
+
+it('can filter text constraint for `not startsWith`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, 0, 5);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'startsWith.inverse', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'not like', $filter . '%')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'like', $filter . '%')->get());
+});
+
+it('can filter text constraint for `endsWith`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, -5);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'endsWith', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'like', '%' . $filter)->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'not like', '%' . $filter)->get());
+});
+
+it('can filter text constraint for `not endsWith`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+    $filter = substr($content, -5);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'endsWith.inverse', $filter)
+        ->assertCanSeeTableRecords(Post::where('content', 'not like', '%' . $filter)->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', 'like', '%' . $filter)->get());
+});
+
+it('can filter text constraint for `equals`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'equals', $content)
+        ->assertCanSeeTableRecords(Post::where('content', $content)->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', '<>', $content)->get());
+});
+
+it('can filter text constraint for `not equals`', function () {
+    $posts = Post::factory(10)->create();
+    $content = $posts->random()->content;
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords($posts)
+        ->queryBuilderTable('content', 'equals.inverse', $content)
+        ->assertCanSeeTableRecords(Post::where('content', '<>', $content)->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', $content)->get());
+});
+
+it('can filter text constraint for `isFilled`', function () {
+    Post::factory(8)->create();
+    Post::factory()->create(['content' => null]);
+    Post::factory()->create(['content' => '']);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords(Post::all())
+        ->queryBuilderTable('content', 'isFilled')
+        ->assertCanSeeTableRecords(Post::where('content', '<>', null)->where('content', '<>', '')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', null)->orWhere('content', '')->get());
+});
+
+it('can filter text constraint for `not isFilled`', function () {
+    Post::factory(8)->create();
+    Post::factory()->create(['content' => null]);
+    Post::factory()->create(['content' => '']);
+
+    livewire(PostsQueryBuilderTable::class)
+        ->assertCanSeeTableRecords(Post::all())
+        ->queryBuilderTable('content', 'isFilled.inverse')
+        ->assertCanSeeTableRecords(Post::where('content', null)->orWhere('content', '')->get())
+        ->assertCanNotSeeTableRecords(Post::where('content', '<>', null)->where('content', '<>', '')->get());
+});
 
 it('can filter records by text constraint in the query builder with modal', function () {
     $posts = Post::factory()->count(10)->create();
